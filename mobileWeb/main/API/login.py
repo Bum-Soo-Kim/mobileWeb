eea830e22863903b36411c7b8bc287987d1b8a55
harry <ez0639@gmail.com>
2021-03-09 17:08:00 +0900

    일반 로그인 구현

--- mobileWeb/main/API/login.py
+++ mobileWeb/main/API/login.py
@@ -2,8 +2,28 @@
 from __future__ import unicode_literals
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from django.contrib.auth import login, authenticate
 import json
 
 def renderPage(request):
     return render(request, 'login.html')
 
+def getLogin(request):
+    result = {'code':'','msg':''}
+
+    try:
+        id = request.POST.get('id')
+        pw = request.POST.get('pw')
+        user = authenticate(username = id, password = pw)
+        if user is None:
+            result['code'] = 0
+            result['msg'] = '아이디 또는 비밀번호를 확인해주세요'
+        else:
+            print(login(request, user))
+            result['code'] = 1
+
+    except Exception as e:
+        result['code'] = e
+        result['msg'] = e
+
+    return HttpResponse(json.dumps(result))
